ec37e6e21e2c1c9372ac7d301b7b2ca01b373f71
oduvan <a.lyabah@checkio.org>
2021-10-31 21:46:11 +0200

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import setup, find_packages
 source_directory = dirname(abspath(__file__))
 
 setup(
-    name='checkio_json_serializer',
-    version='0.1.1',
-    python_requires='>=3.5',
-    description='JSON serilizer for more complex objects',
-    author='CheckiO',
-    author_email='a.lyabah@checkio.org',
-    url='https://github.com/CheckiO/checkio-json-serializer',
+    name="checkio_json_serializer",
+    version="0.1.2",
+    python_requires=">=3.5",
+    description="JSON serilizer for more complex objects",
+    author="CheckiO",
+    author_email="a.lyabah@checkio.org",
+    url="https://github.com/CheckiO/checkio-json-serializer",
     packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
